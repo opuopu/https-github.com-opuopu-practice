361491ef8e6752eadbed2b5fc1b524c83e6f1307
opuopu <43672649+opuopu@users.noreply.github.com>
2023-03-10 20:13:28 +0600

    1 file modified

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 console.log('hello vai apni kmn');
+console.log('hello vai apni kmn');
+console.log('hello vai apni kmn');
+console.log('hello vai apni kmn');
